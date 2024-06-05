91c57b81b54c425ce66ef701b6e4f923905aff33
adomani <adomani@gmail.com>
2024-06-05 16:46:23 +0200

    Update import all file

--- UpdateDeprecations.lean
+++ UpdateDeprecations.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `UpdateDeprecations` library.
 -- Import modules here that should be built as part of the library.
-import «UpdateDeprecations».Main
+import UpdateDeprecations.Main
+import UpdateDeprecations.Test
+import UpdateDeprecations.TUD
