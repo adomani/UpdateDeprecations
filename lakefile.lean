4f9d178f2c4171dc9e03a8bd0f781181e9a0cdc8
adomani <adomani@gmail.com>
2024-06-05 12:40:38 +0200

    Add manifest, require lake, insert update_deprecations

--- lakefile.lean
+++ lakefile.lean
@@ -2,10 +2,22 @@ import Lake
 open Lake DSL
 
 package «UpdateDeprecations» where
-  -- add package configuration options here
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩,
+    ⟨`relaxedAutoImplicit, false⟩
+  ]
+
+require Cli from git "https://github.com/leanprover/lean4-cli" @ "main"
 
 lean_lib «UpdateDeprecations» where
   -- add library configuration options here
+lean_lib Cache
+
+/-- `lake exe update_deprecations` automatically updates deprecations. -/
+lean_exe update_deprecations where
+  srcDir := "scripts"
+  supportInterpreter := true
 
 @[default_target]
 lean_exe «updatedeprecations» where
