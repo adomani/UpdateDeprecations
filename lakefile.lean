af14239f10642f1270d4af151e1ddfdcbdf2dbf9
adomani <adomani@gmail.com>
2024-06-08 08:03:25 +0200

    Use Jon Eugster's fix

--- lakefile.lean
+++ lakefile.lean
@@ -14,9 +14,10 @@ require Cli from git "https://github.com/leanprover/lean4-cli" @ "main"
 lean_lib UpdateDeprecations where
 --  -- add library configuration options here
 
+--source https://leanprover.zulipchat.com/#narrow/stream/270676-lean4/topic/making.20a.20lean.20executable.20available.20in.20a.20project/near/443401065
 /-- `lake exe update_deprecations` automatically updates deprecations. -/
---@[default_target]
+@[default_target]
 lean_exe update_deprecations where
-  srcDir := "UpdateDeprecations"
-  root := `Main
+  -- srcDir := "UpdateDeprecations"
+  root := `UpdateDeprecations.Main
   supportInterpreter := true
